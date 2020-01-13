592766a8286ac058e8c2d51ec9902f6a9744d458
matheus <matheusbritodasilva@gmail.com>
2020-01-13 17:55:33 -0300

    cadastrando e salvando na sessao

--- app/src/main/java/com/example/walterlp/rpgclass/activity/activity/DAO/FirebaseDAO.java
+++ app/src/main/java/com/example/walterlp/rpgclass/activity/activity/DAO/FirebaseDAO.java
@@ -1,7 +1,23 @@
 package com.example.walterlp.rpgclass.activity.activity.DAO;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.walterlp.rpgclass.activity.activity.model.Aluno;
+import com.example.walterlp.rpgclass.activity.activity.model.Professor;
+import com.example.walterlp.rpgclass.activity.activity.model.Sessao;
+import com.example.walterlp.rpgclass.activity.activity.model.TipoUsuario;
+import com.example.walterlp.rpgclass.activity.activity.ui.CadastroActivity;
+import com.example.walterlp.rpgclass.activity.activity.ui.MainActivity;
 import com.example.walterlp.rpgclass.activity.activity.utils.ConstantsUtils;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
@@ -53,4 +69,60 @@ public class FirebaseDAO {
     }
 
 
+    public void register(final String nome, final String email, String  senha, final Sessao sessao, ProgressBar progressBar, AppCompatActivity context) {
+        progressBar.setVisibility(View.VISIBLE);
+        auth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(context, task -> {
+            if (task.isSuccessful()) {
+
+                FirebaseUser user = auth.getCurrentUser();
+                if (sessao.getTipoUsuario().equals(TipoUsuario.ALUNO)){
+                    Aluno aluno  = new Aluno();
+                    aluno.setId(user.getUid());
+                    aluno.setNome(nome);
+                    aluno.setEmail(email);
+                    salvarAluno(aluno);
+
+                }else {
+
+                    Professor professor = new Professor();
+                    professor.setNome(nome);
+                    professor.setEmail(email);
+                    professor.setId(user.getUid());
+                    salvarProfessor(professor);
+                }
+
+                try{
+                    sessao.setId(1L);
+                    sessao.save();
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+
+
+                context.finish();
+                context.startActivity(new Intent(context, MainActivity.class));
+                progressBar.setVisibility(View.GONE);
+
+            } else {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(context, "Erro ao criar usuário",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+
+        });
+    }
+
+    public void salvarAluno(Aluno aluno){
+
+        getFirebase().getDatabase().getReference().child(String.valueOf(ConstantsUtils.ALUNOS_NODE)).child(aluno.getId()).setValue(aluno);
+
+    }
+    public void salvarProfessor(Professor professor){
+        getFirebase().getDatabase().getReference().child(String.valueOf(ConstantsUtils.PROFESSORES_NODE)).child(professor.getId()).setValue(professor);
+    }
 }
--- app/src/main/java/com/example/walterlp/rpgclass/activity/activity/ui/CadastroActivity.java
+++ app/src/main/java/com/example/walterlp/rpgclass/activity/activity/ui/CadastroActivity.java
@@ -6,12 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.walterlp.rpgclass.R;
+import com.example.walterlp.rpgclass.activity.activity.DAO.FirebaseDAO;
+import com.example.walterlp.rpgclass.activity.activity.model.Sessao;
+import com.example.walterlp.rpgclass.activity.activity.model.TipoUsuario;
 import com.example.walterlp.rpgclass.activity.activity.utils.UiUtils;
 
 import java.util.ArrayList;
@@ -21,6 +25,8 @@ public class CadastroActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private EditText editTextNome, editTextEmail, editTextSenha;
     private Button buttonCadastrar;
+    private Switch switchTipo;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,27 +42,32 @@ public class CadastroActivity extends AppCompatActivity {
         editTextEmail = findViewById(R.id.editCadastroEmail);
         editTextNome = findViewById(R.id.editCadastroNome);
         editTextSenha = findViewById(R.id.editCadastroSenha);
+        switchTipo = findViewById(R.id.switc);
+
 
 
         buttonCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
                 List<EditText> editTexts = new ArrayList<>();
                 editTexts.add(editTextEmail);
                 editTexts.add(editTextSenha);
                 editTexts.add(editTextNome);
-                if(UiUtils.validFields(editTexts, getApplicationContext())){
-                    progressBar.setVisibility(View.VISIBLE);
+                if (UiUtils.validFields(editTexts, getApplicationContext())) {
 
+                    Sessao sessao = new Sessao();
+
+                    if(switchTipo.isChecked()){
+                        sessao.setTipoUsuario(TipoUsuario.PROFESSOR);
+                    }else{
+                        sessao.setTipoUsuario(TipoUsuario.ALUNO);
+
+                    }
+
+                    new FirebaseDAO().register(editTextNome, editTextEmail, editTextSenha, sessao, progressBar,CadastroActivity.this);
 
 
                 }
-
-
-
-
 
 
             }
