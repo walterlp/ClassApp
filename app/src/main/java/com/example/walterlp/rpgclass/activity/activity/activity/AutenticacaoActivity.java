abfcfc62d9f0aa7a79452a614f0771d6878dd110
walterlp <walterlp10@hotmail.com>
2020-01-13 12:35:51 -0300

    First

---
+++ app/src/main/java/com/example/walterlp/rpgclass/activity/activity/activity/AutenticacaoActivity.java
@@ -0,0 +1,16 @@
+package com.example.walterlp.rpgclass.activity.activity.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.walterlp.rpgclass.R;
+
+public class AutenticacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autenticacao);
+    }
+}
